63a59bc0fa09493ea090c6473325deb8624ebc04
Pedro Mion <pedromionbraga@gmail.com>
2023-11-28 09:48:28 -0300

    Adicionando comentários

--- SimpsonSequencial.c
+++ SimpsonSequencial.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Definindo variáveis globais que serão utilizadas no código
 double limiteInferior, limiteSuperior;
 int quantidadeIteracoes;
 
 
+// Struct que guarda as informações de integração a, b, h e quantidade de iterações.
+// Será passada como argumento para a função que executa a regra, para ficar mais legível
 typedef struct{
    double limiteInferior;
    double limiteSuperior;
@@ -14,31 +17,42 @@ typedef struct{
 } infosIntegracao;
 
 
+// Função matemática que será integrada
 double funcao(double x) {
    return x;
 }
 
 
+// Função que realiza a aproximação da integral a partir da regra de Simpson
 double regraDeSimpsonSequencial(infosIntegracao infos) {
+    // Criando variáveis locais que serão utilizadas por essa thread
     double soma1 = 0, soma2 = 0, resultado;
 
+    // Calculando o tamanho do intervalo como sendo (b - a) / n
     infos.tamanhoIntervalo = (infos.limiteSuperior-infos.limiteInferior) / infos.quantidadeIteracoes;
+
+    // Criando vetor com tamanho para os valores da função de acordo com a quantidade de iterações,
+    // que será usado no código
     double* valores = (double *) malloc(sizeof(double) * (infos.quantidadeIteracoes+1));
 
+    // Calculando o valor da função para cada valor de x pertencente ao intervalo, de acordo com o número de iterações
     for(int i = 0; i < (infos.quantidadeIteracoes+1); i++) {
         valores[i] = funcao(infos.limiteInferior + i*infos.tamanhoIntervalo);
     }
 
+    // Somatório "par", uma parcela do código, realizado da forma que o método dita
     for(int i = 1; i < ceil(infos.quantidadeIteracoes / 2); i++) {
         soma1 += valores[2 * i];
     }
     soma1 *= 2;
 
+    // Somatório "ímpar", uma parcela do código, realizado da forma que o método dita
     for(int i = 1; i < ceil((infos.quantidadeIteracoes / 2) + 1); i++) {
         soma2 += valores[(2 * i) - 1];
     }
     soma2 *= 4;
 
+    // Usa a fórmula da regra de Simpson para estimar a integral neste intervalo
     resultado = ((infos.tamanhoIntervalo/3) * (funcao(infos.limiteInferior) + funcao(infos.limiteSuperior) + soma1 + soma2));
 
     return resultado;
@@ -49,6 +63,7 @@ int main(int argc, char* argv[]) {
     infosIntegracao infos;
     double resultado;
 
+    // Recuperando as variáveis da linha de comando
     if(argc<4) {
         printf("Digite: %s <limiteInferior> <limiteSuperior> <quantidadeIteracoes>\n", argv[0]);
         return 1;
@@ -58,11 +73,12 @@ int main(int argc, char* argv[]) {
     limiteSuperior = atof(argv[2]);
     quantidadeIteracoes = atoi(argv[3]);
 
-
+    // Setando os valores no struct
     infos.limiteInferior = limiteInferior;
     infos.limiteSuperior = limiteSuperior;
     infos.quantidadeIteracoes = quantidadeIteracoes;
 
+    // Calcula a integral aproximada
     resultado = regraDeSimpsonSequencial(infos);
 
     printf("Valor aproximado da integral definida: %lf\n", resultado);
